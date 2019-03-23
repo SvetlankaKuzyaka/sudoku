fed36bf8fcdc230d23ca40566025ff86ee78e1e5
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2019-03-23 15:46:12 +0300

    add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  mainFunction(matrix);
+  return matrix;
+}
+
+function mainFunction(matrix) {
+  var init = [1,2,3,4,5,6,7,8,9];
+  for (var i = 0; i < 9; i++) {
+      for (var j = 0; j < 9; j++) {
+          if (matrix[i][j] == 0) {
+            var bufer = [];
+            var finish = [];
+            for (var k = 0; k < 9; k++){
+              if ((matrix[i][k] != 0) && bufer.indexOf(matrix[i][k]) == -1) bufer.push(matrix[i][k]);
+              if ((matrix[k][j] != 0) && bufer.indexOf(matrix[k][j]) == -1) bufer.push(matrix[k][j]);
+            }
+            for (var m = i-i%3; m < i-i%3+3; m++){
+              for (var p = j-j%3; p < j-j%3+3; p++){
+              if ((matrix[m][p] != 0) && bufer.indexOf(matrix[m][p]) == -1) bufer.push(matrix[m][p]);
+              }
+            }
+            finish = init.filter(function(item) {
+              if (bufer.indexOf(item) == -1) {return true;}
+              else {return false;}
+            })
+            for (var q = 0; q < finish.length; q++) {
+              matrix[i][j] = finish[q];
+              if (mainFunction(matrix)) return true;
+              matrix[i][j] = 0;
+            }
+            return false;
+          }
+        }
+      }
+     return true;
 }
